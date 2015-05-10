9969ce71deece080d2ab0c0861d12d0f9928086e
Neus Agelet <naushika@macbook-pro-de-neus.home>
2015-05-10 20:52:58 +0200

    [Alta soci] formulari de base i llista de socis

--- app/app.js
+++ app/app.js
@@ -1,12 +1,36 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('cclgApp', [
+  'ngRoute'])
+.controller('cclgAppCtrl', [
+	'$scope', function($scope) {
+	$scope.sociList = [];
+
+    $scope.quotes = [
+    	{id: 10, name:'Familia', value:120},
+    	{id: 20, name:'Adult o familia jubilada', value:100},
+    	{id: 30, name:'Jove o Jubilat', value:80},
+    	{id: 40, name:'Estudiant', value:40},
+    	{id: 50, name:'Protector', value:250},
+    	{id: 60, name:'benefactor', value:500}
+    ];
+
+    
+
+    function Soci(){
+    	this.quota = $scope.quotes[0];
+    	this.actiu = true;
+    }
+
+    $scope.soci = new Soci();
+
+    $scope.addSoci = function() {
+      if ($scope.soci.name) {
+        var soci = angular.copy($scope.soci);
+        $scope.sociList.push(soci);
+        $scope.soci = new Soci();
+      }
+    };
+
+  }]);
